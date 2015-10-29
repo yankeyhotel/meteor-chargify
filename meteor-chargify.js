b8a91313972fba82c6c153cbfce2547c22a52155
Matt McClard <yankeyhotel@gmail.com>
2015-10-29 16:52:58 -0500

    Add getSubscription & createSubscription methods

--- meteor-chargify.js
+++ meteor-chargify.js
@@ -65,3 +65,13 @@ Chargify.prototype.subscriptions = function(callback) {
   var request = "/subscriptions";
   this.get(request, callback);
 };
+
+Chargify.prototype.getSubscription = function(id, callback) {
+  var request = "/subscriptions/" + id;
+  this.get(request, callback);
+};
+
+Chargify.prototype.createSubscription = function(id, callback) {
+  var request = "/subscription";
+  this.post(request, callback);
+}
